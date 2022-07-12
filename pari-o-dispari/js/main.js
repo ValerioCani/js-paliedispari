692f889896416fb21094518885b42e226aea7f6f
Valerio <canivalerio93@gmail.com>
2022-07-12 19:29:32 +0200

    programma completato con le due funzioni

--- pari-o-dispari/js/main.js
+++ pari-o-dispari/js/main.js
@@ -1,21 +1,51 @@
 const myNumber = prompt('inserisci un numero tra 1 e 5');
-
+console.log(myNumber);
+const choice = prompt('digita pari o dispari').toUpperCase();
 //blocco aggressivo di controllo dei dati in ingresso
 if(isNaN(myNumber)){
     alert('INSERISCI UN NUMERO IDIOTA!');
-
+    
 }else if(myNumber<1 || myNumber>5){
     alert('SAI LEGGERE? DEVI INSERIRE UN NUMERO TRA 1 E 5!');
-
-}else{
-    const choice = prompt('digita pari o dispari').toUpperCase();
-
-    if(choice!=='PARI' && choice!=='DISPARI'){
-    alert('HAI SCRITTO MALE!');
-    }
-}
-
-const 
-
     
+}else if(choice!=='PARI' && choice!=='DISPARI'){
+    alert('HAI SCRITTO MALE!');
+  
+};
+
+const compNumber = RandomNumber(1, 5);
+console.log(compNumber);
+
+const sum = myNumber + compNumber;
+
+if(oddEven(sum) == 'even'){
+    if(choice == 'PARI'){
+        alert('Complimenti Hai Vinto!');
+    }else{
+        alert('Mi dispaice Hai Perso');
+    }
+} else {
+    if(choice == 'PARI'){
+        alert('Mi dispaice Hai Perso');
+    }else{
+        alert('Complimenti Hai Vinto!');
+    }
+};
+
+
+function RandomNumber(min, max){
+if (typeof min == "number" && typeof max == "number") {
+    return  Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(randomNumber);
+    }
+};
+
+function oddEven(number){
+    if(number % 2 == 0){
+        return 'even';
+    }
+    else{
+        return 'odd';
+    }
+};
 
