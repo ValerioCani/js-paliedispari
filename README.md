bd5f0ce81aefa10a3725568984b9589e524c2bf4
Valerio <canivalerio93@gmail.com>
2022-07-13 10:02:38 +0200

    trasformato programma pailndroma in funzione, ho letto male la consegna

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ nome repo: js-paliedispari
 Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
-Pari e Dispari
 
+
+Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 Sommiamo i due numeri
--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,23 +1,30 @@
 const parola = prompt('inserisci una parola');
-const myArray = parola.split("");
-console.log(myArray);
 
-const cyclenNmber = Math.floor(myArray.length / 2);
-console.log(cyclenNmber);
+alert (chekcPalindrome(parola));
 
-let found=false;
 
-for( let i=0; i<cyclenNmber; i++){
-    if(myArray[i] !== myArray[myArray.length - 1 - i]){
-        found=false;
+
+function chekcPalindrome(Word){
+    const myArray = Word.split("");
+    console.log(myArray);
+    
+    const cyclenNmber = Math.floor(myArray.length / 2);
+    console.log(cyclenNmber);
+    
+    let found=false;
+    
+    for( let i=0; i<cyclenNmber; i++){
+        if(myArray[i] !== myArray[myArray.length - 1 - i]){
+            found=false;
+        }else{
+            found=true;
+        }
+        console.log(i);
+    };
+    
+    if(found==true){
+        return 'la parola è palindroma';
     }else{
-        found=true;
-    }
-    console.log(i);
-};
-
-if(found==true){
-    alert('la parola è palindroma');
-}else{
-    alert('la parola non è palindroma'); 
+        return 'la parola non è palindroma'; 
+    };
 };
